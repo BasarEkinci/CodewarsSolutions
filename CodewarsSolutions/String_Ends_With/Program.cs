264f3628f814d568a32152555e66cdc851e567fa
BasarEkinci <basarekinci02@outlook.com>
2023-10-05 19:46:53 +0300

    String Ends With? Completed

---
+++ CodewarsSolutions/String_Ends_With/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace String_Ends_With
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public static bool Solution(string str,string ending)
+        {
+            if(ending.Length > str.Length)
+                return false;
+            else
+            {
+                if(str.EndsWith(ending))
+                    return true;
+                else 
+                    return false;
+            }
+        }
+    }
+}
