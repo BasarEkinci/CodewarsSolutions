f8278897a13569e8606303e5cf7cd8224b4f15f0
BasarEkinci <basarekinci02@outlook.com>
2023-10-05 20:18:07 +0300

    A Needle in the Haystack Completed

---
+++ CodewarsSolutions/A_Needle_In_The_Haystack/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A_Needle_In_The_Haystack
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public static string FindNeedle(object[] haystack)
+        {
+            return "found the needle at position " + Array.IndexOf(haystack, "needle");
+        }
+    }
+}
