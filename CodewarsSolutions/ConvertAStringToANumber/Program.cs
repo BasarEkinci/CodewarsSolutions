6c6933988a38e202bf84ec2c0a42339ed95aab4c
BasarEkinci <basarekinci02@outlook.com>
2023-10-05 19:55:47 +0300

    Convert a String to Number Completed

---
+++ CodewarsSolutions/ConvertAStringToANumber/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConvertAStringToANumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public static int StringToNumber(string str)
+        {
+            return Convert.ToInt32(str);
+        }
+    }
+}
