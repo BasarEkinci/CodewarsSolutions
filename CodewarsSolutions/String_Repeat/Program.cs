990247ab532953fd9aefd1d02336e79335de9f48
BasarEkinci <basarekinci02@outlook.com>
2023-10-06 00:19:13 +0300

    String Repeat Completed

---
+++ CodewarsSolutions/String_Repeat/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace String_Repeat
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public static string RepeatString(int n, string s)
+        {
+            return string.Concat(Enumerable.Repeat(s, 5));
+        }
+    }
+}
