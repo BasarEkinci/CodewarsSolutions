f9601edb344f10e0384838b5d47b777b1b68cfea
BasarEkinci <basarekinci02@outlook.com>
2023-10-07 19:22:05 +0300

    Sum of Odd Numbers Completed

---
+++ CodewarsSolutions/Sum_of_Odd_Numbers/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sum_of_Odd_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+        static long RowSumOddNumbers(long n)
+        {
+            return n * n * n;
+        }
+    }
+}
