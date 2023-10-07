ab341186666f5489b4b3ce4855573ead8f9a0c11
BasarEkinci <basarekinci02@outlook.com>
2023-10-07 20:53:12 +0300

    Reverse Words Completed

---
+++ CodewarsSolutions/Reverse_Words/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reverse_Words
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ReverseWords("double  spaces"));
+            Console.ReadLine();
+        }
+
+        static string ReverseWords(string str)
+        {
+            string[] words = str.Split(new char[] { ' ' });
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                char[] wordChars = words[i].ToCharArray();
+                Array.Reverse(wordChars);
+                words[i] = new string(wordChars);
+            }
+
+            string result = string.Join(" ", words);
+
+            return result;
+        }
+    }
+}
