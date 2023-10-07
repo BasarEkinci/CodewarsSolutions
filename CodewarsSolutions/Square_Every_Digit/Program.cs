33cf2761ec3cc85a8eac3e78e1950bd820b64a02
BasarEkinci <basarekinci02@outlook.com>
2023-10-07 18:39:37 +0300

    Square Every Digit Completed

---
+++ CodewarsSolutions/Square_Every_Digit/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Square_Every_Digit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SquareDigits(24);
+            Console.ReadLine();
+        }
+
+        static int SquareDigits(int n)
+        {
+            string strNum = n.ToString();
+            string result = "";
+
+            foreach (char digit in strNum)
+            {
+                int intDigit = int.Parse(digit.ToString());
+                int square = intDigit * intDigit;
+                result += square.ToString();
+            }
+            return int.Parse(result);
+        }
+    }
+}
---
+++ CodewarsSolutions/To_Jaden_Case/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace To_Jaden_Case
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        static string ToJadenCase(string pharase)
+        {
+            if (string.IsNullOrEmpty(pharase))
+            {
+                throw new ArgumentException("An empty or null clause cannot be passed");
+            }
+            string[] words = pharase.Split(' ');
+            for (int i = 0; i < words.Length; i++)
+            {
+                if (!string.IsNullOrEmpty(words[i]))
+                {
+                    char[] wordArray= words[i].ToCharArray();
+                    wordArray[0] = char.ToUpper(wordArray[0]);
+                    words[i] = new string(wordArray);
+                }
+            }
+            return string.Join(" ", words);
+        }
+    }
+}
