a7e8b65478393f103880046593adae23097af097
BasarEkinci <basarekinci02@outlook.com>
2023-10-08 22:25:08 +0300

    List Filtering Completed

---
+++ CodewarsSolutions/List_Filtering/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace List_Filtering
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        static IEnumerable<int> GetIntegersFromList(List<object> listOfItems)
+        {
+            List<int> integers = new List<int>();
+
+            foreach (object item in listOfItems)
+            {
+                if (item is int)
+                {
+                    integers.Add((int)item);
+                }
+            }
+
+            return integers;
+        }
+    }
+}
