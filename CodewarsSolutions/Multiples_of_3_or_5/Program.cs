4a26961644f3eed160a4c860f9519eb087462515
BasarEkinci <basarekinci02@outlook.com>
2023-10-11 19:57:38 +0300

    Multiples of 3 or 5 Completed

--- CodewarsSolutions/Multiples_of_3_or_5/Program.cs
+++ CodewarsSolutions/Multiples_of_3_or_5/Program.cs
@@ -15,13 +15,9 @@ namespace Multiples_of_3_or_5
         
         static int Solution(int value)
         {
-            if (value < 0)
+            int result = 0;
+            if(value > 0) 
             {
-                return -1;
-            }
-            else if(value > 0) 
-            {
-                int result = 0;
                 for (int i = 0; i < value; i++)
                 {
                     if (i % 15 == 0)                   
@@ -31,12 +27,8 @@ namespace Multiples_of_3_or_5
                     else if (i % 5 == 0)
                         result += i; 
                 }
-                return result;
             }
-            else
-            {
-                return 0;
-            }
+            return result;
         }
     }
 }
